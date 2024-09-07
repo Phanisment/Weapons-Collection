57cc34384efb085ac2ad83f61d261fec772788ae
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-07 22:01:04 +0700

    update src/main/java/phanisment/legendary/LegendaryWeaponsDataGenerator.java

--- src/main/java/phanisment/legendary/LegendaryWeaponsDataGenerator.java
+++ src/main/java/phanisment/legendary/LegendaryWeaponsDataGenerator.java
@@ -6,6 +6,6 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 public class LegendaryWeaponsDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-
+		
 	}
 }
