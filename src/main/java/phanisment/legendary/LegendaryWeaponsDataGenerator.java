232afb315cf13559fe91d3b9fce95fd070098f8f
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-07 21:52:18 +0700

    Add files via upload

---
+++ src/main/java/phanisment/legendary/LegendaryWeaponsDataGenerator.java
@@ -0,0 +1,11 @@
+package phanisment.legendary;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LegendaryWeaponsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
