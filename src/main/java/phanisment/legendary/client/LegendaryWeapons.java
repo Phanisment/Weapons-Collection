d77ff53e64eec3303b1edfc15ab379cba719db9f
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-08 11:38:58 +0700

    update src/main/java/phanisment/legendary/client/LegendaryWeapons.java

--- src/main/java/phanisment/legendary/client/LegendaryWeapons.java
+++ src/main/java/phanisment/legendary/client/LegendaryWeapons.java
@@ -1,5 +1,28 @@
 package phanisment.legendary.client;
 
-public class LegendaryWeapons {
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+import phanisment.legendary.vfx.render.AnimatedSlashRenderer;
+import phanisment.legendary.LegendaryWeapons;
+
+public class LegendaryWeapons implements ClientModInitializer {
+	public static final LegendaryWeapons MOD_ID;
+	public static final EntityType<AnimatedSlashEntity> ANIMATED_SLASH_ENTITY = Registry.register(
+		Registries.ENTITY_TYPE,
+		new Identifier(MOD_ID, "animated_slash"),
+		EntityType.Builder.create(AnimatedSlashEntity::new, SpawnGroup.MISC)
+			.setDimensions(0.5F, 0.5F)
+			.build()
+	);
 	
+	@Override
+	public void onInitializeClient() {
+		EntityRendererRegistry.register(ANIMATED_SLASH_ENTITY, AnimatedSlashRenderer::new);
+	}
 }
