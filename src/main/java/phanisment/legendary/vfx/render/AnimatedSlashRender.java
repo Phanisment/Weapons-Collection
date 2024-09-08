02cf1cf190756b7af6ec81ec38ef7a77b937ef99
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-08 12:23:16 +0700

    update src/main/java/phanisment/legendary/vfx/render/AnimatedSlashRender.java

--- src/main/java/phanisment/legendary/vfx/render/AnimatedSlashRender.java
+++ src/main/java/phanisment/legendary/vfx/render/AnimatedSlashRender.java
@@ -7,18 +7,16 @@ import net.minecraft.util.Identifier;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.util.math.Vec3f;
 
-import phanisment.legendary.LegendaryWeapons;
 import phanisment.legendary.vfx.AnimatedSlashEntity;
 
 public class AnimatedSlashRenderer extends EntityRenderer<AnimatedSlashEntity> {
-	private static final LegendaryWeapons MOD_ID;
 	private static final Identifier[] SLASH_TEXTURES = {
-		new Identifier(MOD_ID, "textures/vfx/slash_1.png"),
-		new Identifier(MOD_ID, "textures/vfx/slash_2.png"),
-		new Identifier(MOD_ID, "textures/vfx/slash_3.png"),
-		new Identifier(MOD_ID, "textures/vfx/slash_4.png"),
-		new Identifier(MOD_ID, "textures/vfx/slash_5.png"),
-		new Identifier(MOD_ID, "textures/vfx/slash_6.png")
+		new Identifier("legendary_weapons", "textures/vfx/slash_1.png"),
+		new Identifier("legendary_weapons", "textures/vfx/slash_2.png"),
+		new Identifier("legendary_weapons", "textures/vfx/slash_3.png"),
+		new Identifier("legendary_weapons", "textures/vfx/slash_4.png"),
+		new Identifier("legendary_weapons", "textures/vfx/slash_5.png"),
+		new Identifier("legendary_weapons", "textures/vfx/slash_6.png")
 	};
 	
 	public AnimatedSlashRenderer(EntityRendererFactory.Context context) {
