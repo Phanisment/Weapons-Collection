d0f97b357171131cdab431bf25112ba6a6035157
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-08 11:23:24 +0700

    update src/main/java/phanisment/legendary/LegendaryWeapons.java

--- src/main/java/phanisment/legendary/LegendaryWeapons.java
+++ src/main/java/phanisment/legendary/LegendaryWeapons.java
@@ -6,11 +6,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class LegendaryWeapons implements ModInitializer {
-	public static final String MOD_ID = "legendary-weapons";
+	public static final String MOD_ID = "legendary_weapons";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final Item VFX_ITEM = new Item(new Item.Settings().maxCount(1));
 	
 	@Override
 	public void onInitialize() {
 		LOGGER.info("This world, will by MINE.");
+		
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "slash"), VFX_ITEM);
 	}
 }
