2c0e982c603aab0b6c3baf6a6b544d5ea4487e87
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-15 06:06:04 +0700

    update src/main/java/phanisment/collection/libs/VFX.java

--- src/main/java/phanisment/collection/libs/VFX.java
+++ src/main/java/phanisment/collection/libs/VFX.java
@@ -1,30 +1,39 @@
 package phanisment.collection.libs;
 
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.decoration.DisplayEntity;
+import net.minecraft.entity.decoration.DisplayEntity.ItemDisplayEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.Vec3d;
+import phanisment.collection.mixin.VFXEntityMixin;
 
 public class VFX {
-	public static DisplayEntity.ItemDisplayEntity createItemDisplay(ServerWorld world, ItemStack itemStack, Vec3d position) {
-		DisplayEntity.ItemDisplayEntity itemDisplay = new DisplayEntity.ItemDisplayEntity(EntityType.ITEM_DISPLAY, world);
+
+	public static ItemDisplayEntity createItemDisplay(ServerWorld world, ItemStack itemStack, Vec3d position) {
+		ItemDisplayEntity itemDisplay = new ItemDisplayEntity(world);
 		itemDisplay.setPos(position.x, position.y, position.z);
+
+		// Gunakan accessor untuk mengatur itemStack
+		((VFXEntityMixin) itemDisplay).setItemStack(itemStack);
+
 		NbtCompound nbt = new NbtCompound();
 		itemStack.writeNbt(nbt);
-		itemDisplay.setCustomData(nbt);
+		((VFXEntityMixin) itemDisplay).invokeReadCustomDataFromNbt(nbt);
+
 		world.spawnEntity(itemDisplay);
 		return itemDisplay;
 	}
 
-	public static void animateCustomModelData(DisplayEntity.ItemDisplayEntity itemDisplay, int maxFrame, int ticksPerFrame, Runnable onFinish) {
+	public static void animateCustomModelData(ItemDisplayEntity itemDisplay, int maxFrame, int ticksPerFrame, Runnable onFinish) {
 		new Thread(() -> {
 			try {
 				for (int frame = 1; frame <= maxFrame; frame++) {
-					NbtCompound nbt = itemDisplay.getCustomData();
+					ItemStack itemStack = ((VFXEntityMixin) itemDisplay).getItemStack();
+					NbtCompound nbt = itemStack.getOrCreateNbt();
 					nbt.putInt("CustomModelData", frame);
-					itemDisplay.setCustomData(nbt);
+					itemStack.setNbt(nbt);
+					((VFXEntityMixin) itemDisplay).setItemStack(itemStack);
+
 					Thread.sleep(ticksPerFrame * 50);
 				}
 				onFinish.run();
