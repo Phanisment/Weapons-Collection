373942d58b121f5d28389ce15c0ea4f18f92fdbb
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-14 20:58:07 +0700

    update src/main/java/phanisment/collection/libs/VFX.java

--- src/main/java/phanisment/collection/libs/VFX.java
+++ src/main/java/phanisment/collection/libs/VFX.java
@@ -18,27 +18,4 @@ public class VFX {
 		world.spawnEntity(itemDisplay);
 		return itemDisplay;
 	}
-
-	public static void animateCustomModelData(ItemDisplayEntity itemEntity, int maxFrame, int ticksPerFrame, Runnable onFinish) {
-		ServerWorld world = (ServerWorld) itemEntity.getWorld();
-		ServerTickEvents.END_SERVER_TICK.register(server -> {
-			long tick = world.getServer().getTicks(); // Mengambil jumlah tick saat ini
-			int frame = (int) ((tick / ticksPerFrame) % (maxFrame + 1)); // Menghitung frame berdasarkan tick
-
-			if (frame <= maxFrame) {
-				NbtCompound nbt = new NbtCompound();
-				itemEntity.writeCustomDataToNbt(nbt);
-				nbt.putInt("CustomModelData", frame);
-				itemEntity.readCustomDataFromNbt(nbt);
-			}
-			if (frame == maxFrame) {
-				onFinish.run();
-			}
-		});
-	}
-
-	public static void setRotation(ItemDisplayEntity itemEntity, float pitch, float yaw, float roll) {
-		itemEntity.setYaw(yaw);
-		itemEntity.setPitch(pitch);
-	}
 }
