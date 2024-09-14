b8d77a97e762b22e0752a2f432176c92296a64bb
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-14 21:32:58 +0700

    update src/main/java/phanisment/collection/libs/VFX.java

--- src/main/java/phanisment/collection/libs/VFX.java
+++ src/main/java/phanisment/collection/libs/VFX.java
@@ -10,12 +10,11 @@ import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 
 public class VFX {
 	public static ItemDisplayEntity createItemDisplay(ServerWorld world, ItemStack itemStack, Vec3d position) {
-		ItemDisplayEntity itemDisplay = new ItemDisplayEntity(world, position);
-		itemDisplay.setPos(position.x, position.y, position.z);
-		NbtCompound nbt = new NbtCompound();
-		itemStack.writeNbt(nbt);
-		itemDisplay.getItemDisplayData().setData(nbt);
-		world.spawnEntity(itemDisplay);
-		return itemDisplay;
+	ItemDisplayEntity itemDisplay = new ItemDisplayEntity(world, position);
+	NbtCompound nbt = new NbtCompound();
+	itemStack.writeNbt(nbt);
+	itemDisplay.getItemDisplayData().setData(nbt); // Setting NBT data to the entity's display data
+	world.spawnEntity(itemDisplay);
+	return itemDisplay;
 	}
 }
