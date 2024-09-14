37375435bc7b751acf075d48850c8e80b1cf6336
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-14 17:38:25 +0700

    update src/main/java/phanisment/collection/Weapons.java

--- src/main/java/phanisment/collection/Weapons.java
+++ src/main/java/phanisment/collection/Weapons.java
@@ -1,6 +1,12 @@
 package phanisment.collection;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.Vec3d;
+
+import phanisment.collection.libs.VFX;
 
 public class Weapons implements ModInitializer {
 	@Override
