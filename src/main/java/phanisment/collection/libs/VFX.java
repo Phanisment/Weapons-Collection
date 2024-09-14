94bb1a7b589b6c28c30ea1ecb3db8c841e34efb2
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-14 23:08:08 +0700

    update src/main/java/phanisment/collection/libs/VFX.java

--- src/main/java/phanisment/collection/libs/VFX.java
+++ src/main/java/phanisment/collection/libs/VFX.java
@@ -2,60 +2,35 @@ package phanisment.collection.libs;
 
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.decoration.DisplayEntity;
-import net.minecraft.entity.decoration.DisplayEntity.ItemDisplayEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.Vec3d;
 
 public class VFX {
-
-	public static ItemDisplayEntity createItemDisplay(ServerWorld world, ItemStack itemStack, Vec3d position) {
-		DisplayItem itemDisplay = new DisplayItem(EntityType.ITEM_DISPLAY, world);
+	public static DisplayEntity.ItemDisplayEntity createItemDisplay(ServerWorld world, ItemStack itemStack, Vec3d position) {
+		DisplayEntity.ItemDisplayEntity itemDisplay = new DisplayEntity.ItemDisplayEntity(EntityType.ITEM_DISPLAY, world);
 		itemDisplay.setPos(position.x, position.y, position.z);
-
-		// Mengatur data item ke dalam NBT
 		NbtCompound nbt = new NbtCompound();
 		itemStack.writeNbt(nbt);
 		itemDisplay.setCustomData(nbt);
-
 		world.spawnEntity(itemDisplay);
 		return itemDisplay;
 	}
 
-	public static void animateCustomModelData(ItemDisplayEntity itemDisplay, int maxFrame, int ticksPerFrame, Runnable onFinish) {
+	public static void animateCustomModelData(DisplayEntity.ItemDisplayEntity itemDisplay, int maxFrame, int ticksPerFrame, Runnable onFinish) {
 		new Thread(() -> {
 			try {
 				for (int frame = 1; frame <= maxFrame; frame++) {
-					// Ambil NBT dari item display
 					NbtCompound nbt = itemDisplay.getCustomData();
 					nbt.putInt("CustomModelData", frame);
-
-					// Simpan NBT ke item display
 					itemDisplay.setCustomData(nbt);
-					Thread.sleep(ticksPerFrame * 50); // Tidur untuk durasi animasi
+					Thread.sleep(ticksPerFrame * 50);
 				}
-				onFinish.run(); // Jalankan callback setelah animasi selesai
+				onFinish.run();
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}).start();
-	}
-
-	public static class DisplayItem extends ItemDisplayEntity {
-		public DisplayItem(EntityType<?> type, ServerWorld world) {
-			super(type, world);
-		}
-
-		// Membaca dan menulis data kustom ke NBT
-		public void setCustomData(NbtCompound nbt) {
-			this.readCustomDataFromNbt(nbt);
-		}
-
-		public NbtCompound getCustomData() {
-			NbtCompound nbt = new NbtCompound();
-			this.writeCustomDataToNbt(nbt);
-			return nbt;
-		}
 	}
 }
