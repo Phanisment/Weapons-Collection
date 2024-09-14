eb7af01d58f9500889c4238593c58ad938c64598
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-14 23:10:35 +0700

    update src/main/java/phanisment/collection/mixin/VFXEntityMixin.java

--- src/main/java/phanisment/collection/mixin/VFXEntityMixin.java
+++ src/main/java/phanisment/collection/mixin/VFXEntityMixin.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(DisplayEntity.ItemDisplayEntity.class)
-public class ItemDisplayEntityMixin {
+public class VFXEntityMixin {
 
 	@Shadow
 	private ItemStack itemStack;
