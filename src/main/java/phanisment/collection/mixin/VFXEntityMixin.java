a73d84bdcaf16286b86847fe56a08b907e948051
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-14 23:10:11 +0700

    update src/main/java/phanisment/collection/mixin/VFXEntityMixin.java

---
+++ src/main/java/phanisment/collection/mixin/VFXEntityMixin.java
@@ -0,0 +1,39 @@
+package phanisment.collection.mixin;
+
+import net.minecraft.entity.decoration.DisplayEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(DisplayEntity.ItemDisplayEntity.class)
+public class ItemDisplayEntityMixin {
+
+	@Shadow
+	private ItemStack itemStack;
+
+	@Shadow
+	private void setItemStack(ItemStack stack) {
+		// Shadow method to set item stack
+	}
+
+	@Shadow
+	private void readCustomDataFromNbt(NbtCompound nbt) {
+		// Shadow method to read custom data
+	}
+
+	@Shadow
+	private void writeCustomDataToNbt(NbtCompound nbt) {
+		// Shadow method to write custom data
+	}
+
+	public void setCustomData(NbtCompound nbt) {
+		readCustomDataFromNbt(nbt);
+	}
+
+	public NbtCompound getCustomData() {
+		NbtCompound nbt = new NbtCompound();
+		writeCustomDataToNbt(nbt);
+		return nbt;
+	}
+}
