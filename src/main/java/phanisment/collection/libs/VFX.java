163b92f17c72d703fb9c3f3d13b5a41d01159b69
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-14 20:43:44 +0700

    update src/main/java/phanisment/collection/libs/VFX.java

--- src/main/java/phanisment/collection/libs/VFX.java
+++ src/main/java/phanisment/collection/libs/VFX.java
@@ -10,45 +10,27 @@ import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 
 public class VFX {
 	public static ItemDisplayEntity createItemDisplay(ServerWorld world, ItemStack itemStack, Vec3d position) {
-		// Menggunakan ItemDisplayEntity dengan EntityType dan World
 		ItemDisplayEntity itemDisplay = new ItemDisplayEntity(EntityType.ITEM_DISPLAY, world);
-		
-		// Set posisi entity
 		itemDisplay.setPos(position.x, position.y, position.z);
-
-		// Set ItemStack ke item display menggunakan NBT
 		NbtCompound nbt = new NbtCompound();
 		itemStack.writeNbt(nbt);
-		itemDisplay.readCustomDataFromNbt(nbt); // Membaca data dari NBT untuk item display
-
-		// Spawn entity di dunia
+		itemDisplay.readCustomDataFromNbt(nbt);
 		world.spawnEntity(itemDisplay);
-
 		return itemDisplay;
 	}
 
 	public static void animateCustomModelData(ItemDisplayEntity itemEntity, int maxFrame, int ticksPerFrame, Runnable onFinish) {
 		ServerWorld world = (ServerWorld) itemEntity.getWorld();
-
-		// Menggunakan ServerTickEvents untuk animasi frame-by-frame
 		ServerTickEvents.END_SERVER_TICK.register(server -> {
 			long tick = world.getServer().getTicks(); // Mengambil jumlah tick saat ini
-
 			int frame = (int) ((tick / ticksPerFrame) % (maxFrame + 1)); // Menghitung frame berdasarkan tick
 
 			if (frame <= maxFrame) {
-				// Mengambil NBT dari entity display
 				NbtCompound nbt = new NbtCompound();
 				itemEntity.writeCustomDataToNbt(nbt);
-
-				// Set CustomModelData ke frame saat ini
 				nbt.putInt("CustomModelData", frame);
-
-				// Tulis kembali NBT ke entity display
 				itemEntity.readCustomDataFromNbt(nbt);
 			}
-
-			// Jalankan callback ketika animasi selesai
 			if (frame == maxFrame) {
 				onFinish.run();
 			}
@@ -56,8 +38,7 @@ public class VFX {
 	}
 
 	public static void setRotation(ItemDisplayEntity itemEntity, float pitch, float yaw, float roll) {
-		itemEntity.setYaw(yaw); // Set yaw (horizontal rotation)
-		itemEntity.setPitch(pitch); // Set pitch (vertical rotation)
-		// Note: Roll biasanya tidak disediakan untuk entitas Minecraft, jadi perlu diimplementasikan sendiri jika ingin.
+		itemEntity.setYaw(yaw);
+		itemEntity.setPitch(pitch);
 	}
 }
