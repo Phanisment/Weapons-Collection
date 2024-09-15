660c1fbf9cf90a79942bef7c412c9bf1e8fa1aa3
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-15 20:52:25 +0700

    update src/main/java/phanisment/collection/Weapons.java

--- src/main/java/phanisment/collection/Weapons.java
+++ src/main/java/phanisment/collection/Weapons.java
@@ -13,18 +13,10 @@ public class Weapons implements ModInitializer {
 	public static final String MOD_ID = "phanisments-weapons-collection";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	public static final EntityType<SlashVFXEntity> SLASH_VFX_ENTITY = Registry.register(
-		Registry.ENTITY_TYPE,
-		new Identifier("phanisment", "slash_vfx"),
-		FabricEntityTypeBuilder.create(SpawnGroup.MISC, SlashVFXEntity::new)
-			.dimensions(EntityDimensions.fixed(1.0f, 1.0f))
-			.build()
-	);
-
 	@Override
 	public void onInitialize() {
 		LOGGER.info("7 Sin's will comes"); // Story of the Weapons
 		
-		
+		Entities.registerEntities();
 	}
 }
