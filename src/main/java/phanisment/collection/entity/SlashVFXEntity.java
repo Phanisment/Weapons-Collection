16712016cd034fbb745017c5af5dc40d550bb699
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-15 19:29:16 +0700

    update src/main/java/phanisment/collection/entity/SlashVFXEntity.java

---
+++ src/main/java/phanisment/collection/entity/SlashVFXEntity.java
@@ -0,0 +1,48 @@
+package phanisment.collection.entity;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.world.World;
+
+public class SlashVFXEntity extends Entity {
+	public int lifeTime = 8;
+	public SlashVFXEntity(EntityType<? extends SlashVFXEntity> entityType, World world) {
+		super(entityType, world);
+	}
+
+	@Override
+	public void tick() {
+		super.tick();
+		
+		if (this.lifeTime-- <= 0) {
+			this.discard();
+		}
+	}
+
+	@Override
+	protected void initDataTracker() {
+	}
+
+	@Override
+	protected void readCustomDataFromNbt(NbtCompound nbt) {
+		this.lifeTime = nbt.getInt("LifeTime");
+	}
+
+	@Override
+	protected void writeCustomDataToNbt(NbtCompound nbt) {
+		nbt.putInt("LifeTime", this.lifeTime);
+	}
+
+	@Override
+	public boolean damage(DamageSource source, float amount) {
+		return false;
+	}
+
+	@Override
+	public boolean collides() {
+		return false;
+	}
+}
