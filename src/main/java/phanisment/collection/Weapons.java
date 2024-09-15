03a3c86ece47662bc978a9fde49ebe1d3e0700ff
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-15 19:18:44 +0700

    update src/main/java/phanisment/collection/Weapons.java

--- src/main/java/phanisment/collection/Weapons.java
+++ src/main/java/phanisment/collection/Weapons.java
@@ -1,22 +1,9 @@
 package phanisment.collection;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.player.UseItemCallback;
-import net.minecraft.entity.decoration.DisplayEntity.ItemDisplayEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
-import net.minecraft.util.hit.HitResult;
-import net.minecraft.util.math.Vec3d;
-import net.minecraft.world.World;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import phanisment.collection.libs.VFX;
 
 public class Weapons implements ModInitializer {
 	public static final String MOD_ID = "phanisments-weapons-collection";
@@ -24,14 +11,6 @@ public class Weapons implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		UseItemCallback.EVENT.register((player, world, hand) -> {
-			if (hand == Hand.MAIN_HAND && !world.isClient()) {
-				ItemStack mainHandStack = player.getMainHandStack();
-				if (mainHandStack.getItem() == Items.DIAMOND_SWORD) {
-					return TypedActionResult.success(mainHandStack);
-				}
-			}
-			return TypedActionResult.pass(ItemStack.EMPTY);
-		});
+		LOGGER.info("7 Sin's will comes") // Story of the Weapons
 	}
 }
