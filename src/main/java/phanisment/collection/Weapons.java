5bff7da4276806b5d3ad043d4f74e57e6d7480e2
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-15 07:44:01 +0700

    update src/main/java/phanisment/collection/Weapons.java

--- src/main/java/phanisment/collection/Weapons.java
+++ src/main/java/phanisment/collection/Weapons.java
@@ -1,6 +1,15 @@
 package phanisment.collection;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.UseItemCallback;
+import net.minecraft.entity.decoration.DisplayEntity.ItemDisplayEntity;
+import net.minecraft.item.Items;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +22,23 @@ public class Weapons implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		
+		UseItemCallback.EVENT.register((player, world, hand) -> {
+			if (hand == Hand.MAIN_HAND && !world.isClient()) {
+				if (player.getMainHandStack().getItem() == Items.DIAMOND_SWORD) {
+					createSlashEffect(world, player.getPos(), player.getRotationVec(1.0F));
+				}
+			}
+			return ActionResult.PASS;
+		});
+	}
+	
+	private void createSlashEffect(World world, Vec3d playerPos, Vec3d lookDirection) {
+		if (world instanceof ServerWorld serverWorld) {
+			Vec3d slashPos = playerPos.add(lookDirection.normalize().multiply(2)); // Efek muncul di depan player
+			ItemDisplayEntity slashEffect = VFX.createItemDisplay(serverWorld, Items.DIAMOND_SWORD.getDefaultStack(), slashPos);
+		VFX.animateCustomModelData(slashEffect, 5, 5, () -> {
+				slashEffect.kill();
+			});
+		}
 	}
 }
