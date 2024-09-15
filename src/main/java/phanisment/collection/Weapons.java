e9974de5ba7e12054c0b3bd8f579c8f37c986b4e
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-15 20:50:57 +0700

    update src/main/java/phanisment/collection/Weapons.java

--- src/main/java/phanisment/collection/Weapons.java
+++ src/main/java/phanisment/collection/Weapons.java
@@ -1,19 +1,13 @@
 package phanisment.collection;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
-import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
-import net.minecraft.entity.EntityDimensions;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.SpawnGroup;
-import net.minecraft.util.Identifier;
-import net.minecraft.registry.Registry;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import phanisment.collection.entity.SlashVFXEntity;
-import phanisment.collection.client.renderer.SlashVFXEntityRenderer;
+import com.example.slashvfx.registry.ModEntities;
+import phanisment.collection.entity.SlashEntity;
+import phanisment.collection.client.renderer.SlashEntityRenderer;
 
 public class Weapons implements ModInitializer {
 	public static final String MOD_ID = "phanisments-weapons-collection";
@@ -31,6 +25,6 @@ public class Weapons implements ModInitializer {
 	public void onInitialize() {
 		LOGGER.info("7 Sin's will comes"); // Story of the Weapons
 		
-		EntityRendererRegistry.register(SLASH_VFX_ENTITY, SlashVFXEntityRenderer::new);
+		
 	}
 }
