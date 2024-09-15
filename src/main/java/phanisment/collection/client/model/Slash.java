82fd98e83763674a32cc4d141af79a8c5ec0ab01
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-15 20:34:06 +0700

    update src/main/java/phanisment/collection/client/model/Slash.java

--- src/main/java/phanisment/collection/client/model/Slash.java
+++ src/main/java/phanisment/collection/client/model/Slash.java
@@ -1,33 +1,27 @@
 package phanisment.collection.client.model;
 
-import phanisment.collection.entity.SlashVFXEntity;
-import net.minecraft.client.model.*;
-import net.minecraft.client.render.entity.model.EntityModel;
+import net.minecraft.client.model.ModelPart;
 import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.entity.model.EntityModel;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
 
-public class Slash extends EntityModel<SlashVFXEntity> {
+public class SlashModel extends EntityModel<Entity> {
 	private final ModelPart Head;
 
-	public Slash(ModelPart root) {
+	public SlashModel(ModelPart root) {
 		this.Head = root.getChild("Head");
 	}
 
 	public static TexturedModelData getTexturedModelData() {
 		ModelData modelData = new ModelData();
 		ModelPartData modelPartData = modelData.getRoot();
-		modelPartData.addChild("Head", 
-			ModelPartBuilder.create()
-				.uv(0, 0)
-				.cuboid(-16.0F, 0.0F, -16.0F, 32.0F, 0.0F, 32.0F, new Dilation(0.0F)), 
-			ModelTransform.pivot(0.0F, 24.0F, 0.0F)
-		);
+		modelPartData.addChild("Head", ModelPartBuilder.create().uv(-32, 0).cuboid(-16.0F, 0.0F, -16.0F, 32.0F, 0.0F, 32.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 24.0F, 0.0F));
 		return TexturedModelData.of(modelData, 64, 64);
 	}
 
 	@Override
-	public void setAngles(SlashVFXEntity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
-	}
+	public void setAngles(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {}
 
 	@Override
 	public void render(MatrixStack matrices, VertexConsumer vertexConsumer, int light, int overlay, float red, float green, float blue, float alpha) {
