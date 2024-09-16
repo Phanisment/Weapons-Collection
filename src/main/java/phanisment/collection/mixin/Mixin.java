837a89a497d985dbaaee63504b23b4203a62fe65
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-16 13:48:26 +0700

    update src/main/java/phanisment/collection/mixin/Mixin.java

--- src/main/java/phanisment/collection/mixin/Mixin.java
+++ src/main/java/phanisment/collection/mixin/Mixin.java
@@ -1,7 +1,6 @@
 package phanisment.collection.mixin;
 
 import net.minecraft.server.MinecraftServer;
-import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
