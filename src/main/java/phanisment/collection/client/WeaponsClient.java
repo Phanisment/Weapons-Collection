c0e6a055bfab6cb4563eeb1ddd947ef87643a3b6
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-16 10:14:59 +0700

    update src/main/java/phanisment/collection/client/WeaponsClient.java

--- src/main/java/phanisment/collection/client/WeaponsClient.java
+++ src/main/java/phanisment/collection/client/WeaponsClient.java
@@ -2,12 +2,19 @@ package phanisment.collection.client;
 
 import net.fabricmc.api.ClientModInitializer;
 
+import phanisment.collection.client.render.SlashRender;
 import phanisment.collection.client.model.SlashModel;
 import phanisment.collection.entity.SlashEntity;
 import phanisment.collection.Weapons;
 
+@Environment(EnvType.CLIENT)
 public class WeaponsClient implements ClientModInitializer {
+	public static final EntityModelLayer SLASH_LAYER = new EntityModelLayer(new Identifier(Weapons.MOD_ID, "slash"), "main");
+
 	@Override
 	public void onInitializeClient() {
+		EntityRendererRegistry.register(Weapons.SLASH, (context) -> {
+			return new SlashRender(context);
+		});
 	}
 }
