98a9971d34acc24242ddfdc6cb486c8dce34f33a
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-16 11:06:01 +0700

    update src/main/java/phanisment/collection/client/render/SlashRender.java

--- src/main/java/phanisment/collection/client/render/SlashRender.java
+++ src/main/java/phanisment/collection/client/render/SlashRender.java
@@ -5,6 +5,10 @@ import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
+import net.minecraft.client.render.entity.model.EntityModelLayers;
+import net.minecraft.client.render.entity.model.EntityModelLayer;
+import net.minecraft.client.render.entity.model.EntityModelLayerRegistry;
+import net.minecraft.client.render.OverlayTexture;
 
 import phanisment.collection.Weapons;
 import phanisment.collection.entity.SlashEntity;
@@ -12,7 +16,9 @@ import phanisment.collection.client.model.SlashModel;
 import phanisment.collection.client.WeaponsClient;
 
 public class SlashRender extends EntityRenderer<SlashEntity> {
+
 	private final SlashModel model;
+
 	public SlashRender(EntityRendererFactory.Context context) {
 		super(context);
 		this.model = new SlashModel(context.getPart(WeaponsClient.SLASH_LAYER));
@@ -25,7 +31,7 @@ public class SlashRender extends EntityRenderer<SlashEntity> {
 
 	@Override
 	public void render(SlashEntity entity, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light) {
-		model.setAngles(entity, yaw, tickDelta);
-		model.render(matrices, vertexConsumers.getBuffer(model.getLayer(getTexture(entity))), light, OverlayTexture.DEFAULT_UV);
+		this.model.setAngles(entity, 0, 0, 0, yaw, 0);
+		this.model.render(matrices, vertexConsumers.getBuffer(this.model.getLayer(getTexture(entity))), light, OverlayTexture.DEFAULT_UV);
 	}
 }
