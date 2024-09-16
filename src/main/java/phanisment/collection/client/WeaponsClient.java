51b145c2f67f1c080fda4ac2fef419d24d93be9e
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-16 11:48:29 +0700

    update src/main/java/phanisment/collection/client/WeaponsClient.java

--- src/main/java/phanisment/collection/client/WeaponsClient.java
+++ src/main/java/phanisment/collection/client/WeaponsClient.java
@@ -1,22 +1,10 @@
 package phanisment.collection.client;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
-import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
-import net.minecraft.client.render.entity.model.EntityModelLayer;
-import net.minecraft.util.Identifier;
-
-import phanisment.collection.client.render.SlashRender;
-import phanisment.collection.client.model.SlashModel;
-import phanisment.collection.entity.SlashEntity;
-import phanisment.collection.Weapons;
 
 public class WeaponsClient implements ClientModInitializer {
-	public static final EntityModelLayer SLASH_LAYER = new EntityModelLayer(new Identifier(Weapons.MOD_ID, "slash"), "main");
 
 	@Override
 	public void onInitializeClient() {
-		EntityRendererRegistry.register(Weapons.SLASH, SlashRender::new);
-		EntityModelLayerRegistry.registerModelLayer(SLASH_LAYER, SlashModel::getTexturedModelData);
 	}
 }
