15f9d2326a8285e1438edb76107549ffc35f3c0a
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-16 09:31:33 +0700

    update src/main/java/phanisment/collection/entity/SlashEntity.java

--- src/main/java/phanisment/collection/entity/SlashEntity.java
+++ src/main/java/phanisment/collection/entity/SlashEntity.java
@@ -4,7 +4,7 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.world.World;
 
-public abstract class SlashEntity extends Entity {
+public class SlashEntity extends Entity {
 	public SlashEntity(EntityType<? extends Entity> entityType, World world) {
 		super(entityType, world);
 	}
