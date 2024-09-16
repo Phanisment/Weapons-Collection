8f8c9ab168bce22f495d85b35804a8b4c330a8e7
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-16 09:18:52 +0700

    update src/main/java/phanisment/collection/client/WeaponsClient.java

---
+++ src/main/java/phanisment/collection/client/WeaponsClient.java
@@ -0,0 +1,13 @@
+package phanisment.collection.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+import phanisment.collection.client.model.SlashModel;
+import phanisment.collection.entity.SlashEntity;
+import phanisment.collection.Weapons;
+
+public class WeaponsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+	}
+}
