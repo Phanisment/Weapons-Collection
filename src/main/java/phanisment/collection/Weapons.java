63e06a67d26cacc274b24ef164aa7ee0801925a5
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-16 09:26:06 +0700

    update src/main/java/phanisment/collection/Weapons.java

--- src/main/java/phanisment/collection/Weapons.java
+++ src/main/java/phanisment/collection/Weapons.java
@@ -1,6 +1,12 @@
 package phanisment.collection;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.EntityType.Builder;
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.Registries;
+import net.minecraft.util.Identifier;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +16,12 @@ import phanisment.collection.entity.SlashEntity;
 public class Weapons implements ModInitializer {
 	public static final String MOD_ID = "phanisments-weapons-collection";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	public static final EntityType<SlashEntity> SLASH = Registry.register(
+		Registries.ENTITY_TYPE,
+		Identifier.of("phanisment", "slash"),
+		EntityType.Builder.create(SlashEntity::new, SpawnGroup.MISC).dimensions(0.5f, 0.5f).build("slash")
+	);
 
 	@Override
 	public void onInitialize() {
