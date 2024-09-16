4d9ff2cf2827261a9d2953e7cd2868238532176f
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-16 07:15:23 +0700

    update src/main/java/phanisment/collection/client/Weapons.java

--- src/main/java/phanisment/collection/client/Weapons.java
+++ src/main/java/phanisment/collection/client/Weapons.java
@@ -1,7 +1,15 @@
 package phanisment.collection.client;
 
-import phanisment.collection.client.renderer.EntityRenderers;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
+import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.client.render.entity.MobEntityRenderer;
+import net.minecraft.util.Identifier;
+
+import phanisment.collection.client.model.SlashModel;
+import phanisment.collection.entity.SlashEntity;
+import phanisment.collection.Weapons;
 
 public class Weapons implements ClientModInitializer {
 	@Override
