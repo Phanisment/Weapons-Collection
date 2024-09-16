16cecfee08387762d00f87014ca7fa775fdd1579
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-16 09:00:36 +0700

    update src/main/java/phanisment/collection/entity/SlashEntity.java

--- src/main/java/phanisment/collection/entity/SlashEntity.java
+++ src/main/java/phanisment/collection/entity/SlashEntity.java
@@ -2,53 +2,9 @@ package phanisment.collection.entity;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.util.hit.HitResult;
 import net.minecraft.world.World;
 
 public class SlashEntity extends Entity {
-	public SlashEntity(EntityType<? extends SlashEntity> entityType, World world) {
+	public SlashEntity(EntityType<? extends Entity> entityType, World world) {
 		super(entityType, world);
-	}
-	@Override
-	protected void initDataTracker() {
-	}
-
-	@Override
-	protected void readCustomDataFromNbt(NbtCompound nbt) {
-	}
-
-	@Override
-	protected void writeCustomDataToNbt(NbtCompound nbt) {
-	}
-
-	@Override
-	public boolean damage(DamageSource source, float amount) {
-		return false;
-	}
-
-	@Override
-	public boolean collides() {
-		return false;
-	}
-
-	@Override
-	public boolean isPushable() {
-		return false;
-	}
-
-	@Override
-	protected void pushAway(Entity entity) {
-	}
-
-	@Override
-	public HitResult raycast(double maxDistance, float tickDelta, boolean includeFluids) {
-		return null;
-	}
-
-	@Override
-	public void tick() {
-		super.tick();
-	}
 }
