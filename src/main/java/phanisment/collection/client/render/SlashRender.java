7b6feac81d7089a496c51d975143433f10dcac96
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-16 12:07:22 +0700

    update src/main/java/phanisment/collection/client/render/SlashRender.java

--- src/main/java/phanisment/collection/client/render/SlashRender.java
+++ src/main/java/phanisment/collection/client/render/SlashRender.java
@@ -1,4 +1,30 @@
 package phanisment.collection.client.render;
 
-public class SlashRender{
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.client.render.entity.MobEntityRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.Identifier;
+
+import phanisment.collection.client.model.ModelLayer
+import phanisment.collection.client.model.SlashModel;
+import phanisment.collection.entity.SlashEntity;
+import phanisment.collection.Weapons;
+
+public class SlashRender extends MobEntityRenderer<SlashEntity, SlashModel<SlashEntity>> {
+	private static final Identifier TEXTURE = new Identifier(Weapons.MOD_ID, "textures/vfx/slash.png");
+
+	public PorcupineRenderer(EntityRendererFactory.Context context) {
+		super(context, new SlashModel<>(context.getPart(ModelLayer.SLASH)), 0.6f);
+	}
+
+	@Override
+	public Identifier getTexture(SlashEntity entity) {
+		return TEXTURE;
+	}
+
+	@Override
+	public void render(SlashEntity mobEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i) {
+		super.render(mobEntity, f, g, matrixStack, vertexConsumerProvider, i);
+	}
 }
