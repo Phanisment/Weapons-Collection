7e617a4539ea46cc73ae16bbab148eb8c7b1c971
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-16 07:11:35 +0700

    update src/main/java/phanisment/collection/entity/SlashEntity.java

--- src/main/java/phanisment/collection/entity/SlashEntity.java
+++ src/main/java/phanisment/collection/entity/SlashEntity.java
@@ -2,32 +2,53 @@ package phanisment.collection.entity;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.util.hit.HitResult;
 import net.minecraft.world.World;
 
 public class SlashEntity extends Entity {
-	public int animationFrame = 0;
-	public final int maxAnimationFrames = 10;
-
 	public SlashEntity(EntityType<? extends SlashEntity> entityType, World world) {
 		super(entityType, world);
+	}
+	@Override
+	protected void initDataTracker() {
+	}
+
+	@Override
+	protected void readCustomDataFromNbt(NbtCompound nbt) {
+	}
+
+	@Override
+	protected void writeCustomDataToNbt(NbtCompound nbt) {
+	}
+
+	@Override
+	public boolean damage(DamageSource source, float amount) {
+		return false;
+	}
+
+	@Override
+	public boolean collides() {
+		return false;
+	}
+
+	@Override
+	public boolean isPushable() {
+		return false;
+	}
+
+	@Override
+	protected void pushAway(Entity entity) {
+	}
+
+	@Override
+	public HitResult raycast(double maxDistance, float tickDelta, boolean includeFluids) {
+		return null;
 	}
 
 	@Override
 	public void tick() {
 		super.tick();
-		animationFrame++;
-		if (animationFrame >= maxAnimationFrames) {
-			this.remove(RemovalReason.DISCARDED);
-		}
 	}
-
-	@Override
-	protected void initDataTracker() {}
-
-	@Override
-	protected void readCustomDataFromNbt(NbtCompound nbt) {}
-
-	@Override
-	protected void writeCustomDataToNbt(NbtCompound nbt) {}
 }
