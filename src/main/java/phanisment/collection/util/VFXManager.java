00d97f6b4c9a3e2fb875f65c513d5676ee0591af
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-16 19:57:37 +0700

    update src/main/java/phanisment/collection/util/VFXManager.java

--- src/main/java/phanisment/collection/util/VFXManager.java
+++ src/main/java/phanisment/collection/util/VFXManager.java
@@ -6,12 +6,10 @@ import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 
-import phanisment.collection.util.ItemDisplay;
-
 public class VFXManager {
 	public static void spawnVFX(ServerWorld world, BlockPos pos, ItemStack itemStack) {
 		ItemDisplayEntity itemDisplayEntity = new ItemDisplayEntity(world, pos.getX(), pos.getY(), pos.getZ());
-		ItemDisplay entity = itemDisplayEntity.setStack(itemStack);
-		world.spawnEntity(entity);
+		itemDisplayEntity.setStack(itemStack);
+		world.spawnEntity(itemDisplayEntity);
 	}
 }
