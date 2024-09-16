433d06729d0b9433264db272a64564c188bfef5e
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-16 11:06:48 +0700

    update src/main/java/phanisment/collection/client/model/SlashModel.java

--- src/main/java/phanisment/collection/client/model/SlashModel.java
+++ src/main/java/phanisment/collection/client/model/SlashModel.java
@@ -10,18 +10,22 @@ import phanisment.collection.entity.SlashEntity;
 
 public class SlashModel extends EntityModel<SlashEntity> {
 	private final ModelPart Head;
+
 	public SlashModel(ModelPart root) {
 		this.Head = root.getChild("Head");
 	}
+
 	public static TexturedModelData getTexturedModelData() {
 		ModelData modelData = new ModelData();
 		ModelPartData modelPartData = modelData.getRoot();
 		ModelPartData Head = modelPartData.addChild("Head", ModelPartBuilder.create().uv(-32, 0).cuboid(-16.0F, 0.0F, -16.0F, 32.0F, 0.0F, 32.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 24.0F, 0.0F));
 		return TexturedModelData.of(modelData, 64, 64);
 	}
+
 	@Override
 	public void setAngles(SlashEntity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
 	}
+
 	@Override
 	public void render(MatrixStack matrices, VertexConsumer vertexConsumer, int light, int overlay, float red, float green, float blue, float alpha) {
 		Head.render(matrices, vertexConsumer, light, overlay, red, green, blue, alpha);
