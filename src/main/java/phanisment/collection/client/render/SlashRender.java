d61d980c2439526c8ab3d75b33bd5cf0e896e28d
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-16 10:59:43 +0700

    update src/main/java/phanisment/collection/client/render/SlashRender.java

--- src/main/java/phanisment/collection/client/render/SlashRender.java
+++ src/main/java/phanisment/collection/client/render/SlashRender.java
@@ -1,8 +1,8 @@
 package phanisment.collection.client.render;
 
 import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.client.render.entity.EntityRendererFactory;
-import net.minecraft.client.render.entity.MobEntityRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
 
@@ -11,13 +11,21 @@ import phanisment.collection.entity.SlashEntity;
 import phanisment.collection.client.model.SlashModel;
 import phanisment.collection.client.WeaponsClient;
 
-public class SlashRender extends MobEntityRenderer<SlashEntity, SlashModel> {
+public class SlashRender extends EntityRenderer<SlashEntity> {
+	private final SlashModel model;
 	public SlashRender(EntityRendererFactory.Context context) {
-		super(context, new SlashModel(context.getPart(WeaponsClient.SLASH_LAYER)), 0.5f);
+		super(context);
+		this.model = new SlashModel(context.getPart(WeaponsClient.SLASH_LAYER));
 	}
 
 	@Override
 	public Identifier getTexture(SlashEntity entity) {
 		return new Identifier(Weapons.MOD_ID, "textures/vfx/slash.png");
+	}
+
+	@Override
+	public void render(SlashEntity entity, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light) {
+		model.setAngles(entity, yaw, tickDelta);
+		model.render(matrices, vertexConsumers.getBuffer(model.getLayer(getTexture(entity))), light, OverlayTexture.DEFAULT_UV);
 	}
 }
