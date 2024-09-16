091153ed95d2508fe21e8b971d7c3cef32790757
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-16 12:29:31 +0700

    update src/main/java/phanisment/collection/client/render/SlashRender.java

--- src/main/java/phanisment/collection/client/render/SlashRender.java
+++ src/main/java/phanisment/collection/client/render/SlashRender.java
@@ -1,21 +1,19 @@
-package phanisment.collection.client.render;
-
-import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.client.render.entity.EntityRendererFactory;
-import net.minecraft.client.render.entity.MobEntityRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
 
-import phanisment.collection.client.model.ModelLayer;
 import phanisment.collection.client.model.SlashModel;
 import phanisment.collection.entity.SlashEntity;
 import phanisment.collection.Weapons;
 
-public class SlashRender extends MobEntityRenderer<SlashEntity, SlashModel<SlashEntity>> {
+public class SlashRender extends EntityRenderer<SlashEntity> {
 	private static final Identifier TEXTURE = new Identifier(Weapons.MOD_ID, "textures/vfx/slash.png");
+	private final SlashModel<SlashEntity> model;
 
 	public SlashRender(EntityRendererFactory.Context context) {
-		super(context, new SlashModel<>(context.getPart(ModelLayer.SLASH)), 0.6f);
+		super(context);
+		this.model = new SlashModel<>(context.getPart(ModelLayer.SLASH));
 	}
 
 	@Override
@@ -24,7 +22,8 @@ public class SlashRender extends MobEntityRenderer<SlashEntity, SlashModel<Slash
 	}
 
 	@Override
-	public void render(SlashEntity mobEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i) {
-		super.render(mobEntity, f, g, matrixStack, vertexConsumerProvider, i);
+	public void render(SlashEntity entity, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light) {
+		model.render(matrices, vertexConsumers.getBuffer(model.getLayer(TEXTURE)), light, OverlayTexture.DEFAULT_UV, 1.0F, 1.0F, 1.0F, 1.0F);
+		super.render(entity, yaw, tickDelta, matrices, vertexConsumers, light);
 	}
 }
