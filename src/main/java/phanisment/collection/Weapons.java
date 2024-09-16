5214d06341758d8d2f3f0ff112e536b1147b9ba3
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-16 09:53:49 +0700

    update src/main/java/phanisment/collection/Weapons.java

--- src/main/java/phanisment/collection/Weapons.java
+++ src/main/java/phanisment/collection/Weapons.java
@@ -1,5 +1,6 @@
 package phanisment.collection;
 
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
@@ -14,13 +15,13 @@ import org.slf4j.LoggerFactory;
 import phanisment.collection.entity.SlashEntity;
 
 public class Weapons implements ModInitializer {
-	public static final String MOD_ID = "phanisments-weapons-collection";
+	public static final String MOD_ID = "phanisment";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	public static final EntityType<SlashEntity> SLASH = Registry.register(
-		Registries.ENTITY_TYPE,
-		new Identifier("phanisment", "slash"),
-		EntityType.Builder.create(SlashEntity::new, SpawnGroup.MISC).setDimensions(EntityDimensions.fixed(0.5f, 0.5f)).build();
+	public static final EntityType<SlashEntity> SLASH = Registry.register(Registries.ENTITY_TYPE,
+		new Identifier(MOD_ID, "slash"),
+		FabricEntityTypeBuilder.create(SpawnGroup.MISC, SlashEntity::new)
+			.dimensions(EntityDimensions.fixed(0.5f, 0.5f)).build()
 	);
 
 	@Override
