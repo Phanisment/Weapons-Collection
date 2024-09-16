d5bc1b158c74af4ca465b29fe65106203672999b
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-16 13:00:39 +0700

    update src/main/java/phanisment/collection/client/WeaponsClient.java

--- src/main/java/phanisment/collection/client/WeaponsClient.java
+++ src/main/java/phanisment/collection/client/WeaponsClient.java
@@ -1,10 +1,19 @@
 package phanisment.collection.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+
+import phanisment.collection.client.model.ModelLayer;
+import phanisment.collection.client.model.SlashModel;
+import phanisment.collection.client.render.SlashRender;
+import phanisment.collection.entity.Entities;
 
 public class WeaponsClient implements ClientModInitializer {
 
 	@Override
 	public void onInitializeClient() {
+		EntityRendererRegistry.register(Entities.SLASH, SlashRender::new);
+		EntityModelLayerRegistry.registerModelLayer(ModelLayer.SLASH_LAYER, SlashModel::getTexturedModelData);
 	}
 }
