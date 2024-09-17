68df825e524d66dddf12ebe9f9931338d9f2bfcc
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-17 07:20:26 +0700

    update src/main/java/phanisment/collection/Weapons.java

--- src/main/java/phanisment/collection/Weapons.java
+++ src/main/java/phanisment/collection/Weapons.java
@@ -5,12 +5,14 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import phanisment.collection.event.PlayerEvent;
+
 public class Weapons implements ModInitializer {
 	public static final String MOD_ID = "phanisment";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		
+		PlayerEvent.registerEvents();
 	}
 }
