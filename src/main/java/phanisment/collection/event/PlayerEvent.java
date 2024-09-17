ca46899be8924fcb446f2e8c1afab36485b2f9b1
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-17 19:38:58 +0700

    update src/main/java/phanisment/collection/event/PlayerEvent.java

--- src/main/java/phanisment/collection/event/PlayerEvent.java
+++ src/main/java/phanisment/collection/event/PlayerEvent.java
@@ -17,10 +17,9 @@ public class PlayerEvent {
 				if (heldItem.getItem() == Items.DIAMOND_SWORD) {
 					Vec3d playerPos = player.getPos();
 					Vec3d lookVec = player.getRotationVec(1.0F);
-					Vec3d spawnPos = playerPos.add(lookVec.multiply(2.0));
-					ItemStack displayItem = new ItemStack(Items.DIAMOND_SWORD);
-					VFXManager.spawnVFX(serverWorld, displayItem, spawnPos);
-
+					Vec3d spawnPos = playerPos.add(lookVec.multiply(2.0)).add(0, 0.5, 0);
+					ItemStack displayItem = new ItemStack(Items.PAPER);
+					VFXManager.spawnVFXFacingPlayer(serverWorld, displayItem, spawnPos);
 					return ActionResult.SUCCESS;
 				}
 			}
