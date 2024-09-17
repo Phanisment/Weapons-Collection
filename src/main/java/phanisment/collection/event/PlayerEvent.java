80dc6c97338e541db8919d0000db4f189c65bde8
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-17 07:14:21 +0700

    update src/main/java/phanisment/collection/event/PlayerEvent.java

--- src/main/java/phanisment/collection/event/PlayerEvent.java
+++ src/main/java/phanisment/collection/event/PlayerEvent.java
@@ -1,14 +1,10 @@
 package phanisment.collection.event;
 
-import net.fabricmc.fabric.api.event.player.UseItemCallback;
 import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
 import net.minecraft.item.Items;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.server.world.ServerWorld;
 
 import phanisment.collection.util.VFXManager;
@@ -21,7 +17,7 @@ public class PlayerEvent {
 				if (heldItem.getItem() == Items.DIAMOND_SWORD) {
 					Vec3d playerPos = player.getPos();
 					Vec3d lookVec = player.getRotationVec(1.0F);
-					BlockPos spawnPos = new BlockPos(playerPos.add(lookVec.multiply(2.0)));
+					Vec3d spawnPos = playerPos.add(lookVec.multiply(2.0));
 					ItemStack displayItem = new ItemStack(Items.DIAMOND_SWORD);
 					VFXManager.spawnVFX(serverWorld, displayItem, spawnPos);
 
