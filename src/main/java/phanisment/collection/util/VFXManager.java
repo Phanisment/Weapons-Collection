ec04e189cee6acfe23cad6ff078b0d5823f9ea6e
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-17 18:45:14 +0700

    update src/main/java/phanisment/collection/util/VFXManager.java

--- src/main/java/phanisment/collection/util/VFXManager.java
+++ src/main/java/phanisment/collection/util/VFXManager.java
@@ -6,11 +6,23 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.Vec3d;
 
+import java.lang.reflect.Method;
+
 public class VFXManager {
 	public static void spawnVFX(ServerWorld world, ItemStack itemStack, Vec3d pos) {
 		ItemDisplayEntity itemDisplayEntity = new ItemDisplayEntity(EntityType.ITEM_DISPLAY, world);
 		itemDisplayEntity.setPos(pos.x, pos.y, pos.z);
-		ItemDisplay.setItemStack(itemDisplayEntity, itemStack);
+		setItemStack(itemDisplayEntity, itemStack);
 		world.spawnEntity(itemDisplayEntity);
+	}
+
+	private static void setItemStack(ItemDisplayEntity itemDisplayEntity, ItemStack itemStack) {
+		try {
+			Method setItemStackMethod = ItemDisplayEntity.class.getDeclaredMethod("setItemStack", ItemStack.class);
+			setItemStackMethod.setAccessible(true);
+			setItemStackMethod.invoke(itemDisplayEntity, itemStack);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
