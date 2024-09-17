bac1b5819c11d91260c013e020a89f63c7697ad1
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-17 22:15:00 +0700

    update src/main/java/phanisment/collection/event/PlayerEvent.java

--- src/main/java/phanisment/collection/event/PlayerEvent.java
+++ src/main/java/phanisment/collection/event/PlayerEvent.java
@@ -1,27 +1,25 @@
 package phanisment.collection.event;
 
-import net.fabricmc.fabric.api.event.player.UseItemCallback;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.math.Vec3d;
-import net.minecraft.item.ItemStack;
+import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
 import net.minecraft.item.Items;
+import net.minecraft.util.ActionResult;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.server.world.ServerWorld;
 
 import phanisment.collection.util.VFXManager;
 
 public class PlayerEvent {
 	public static void registerEvents() {
-		UseItemCallback.EVENT.register((player, world, hand) -> {
-			if (hand == Hand.MAIN_HAND && world instanceof ServerWorld serverWorld) {
+		AttackEntityCallback.EVENT.register((player, world, hand, entity, hitResult) -> {
+			if (world instanceof ServerWorld serverWorld) {
 				ItemStack heldItem = player.getMainHandStack();
 				if (heldItem.getItem() == Items.DIAMOND_SWORD) {
 					Vec3d playerPos = player.getPos();
 					Vec3d lookVec = player.getRotationVec(1.0F);
-					Vec3d spawnPos = playerPos.add(lookVec.multiply(2.0)).add(0, 1, 0);
-					ItemStack displayItem = new ItemStack(Items.PAPER);
-					float yaw = player.getYaw();
-					VFXManager.spawnVFXFacingPlayer(serverWorld, displayItem, spawnPos, yaw);
+					Vec3d spawnPos = playerPos.add(lookVec.multiply(2.0)).add(0, 0.5, 0);
+					ItemStack displayItem = new ItemStack(Items.DIAMOND_SWORD);
+					VFXManager.spawnVFXFacingPlayer(serverWorld, displayItem, spawnPos, playerPos);
 					return ActionResult.SUCCESS;
 				}
 			}
