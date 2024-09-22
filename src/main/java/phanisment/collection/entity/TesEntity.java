8862b9c3ca7166b898e23ceaf4df3c109ee5bf00
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-22 22:19:43 +0700

    Update TesEntity.java

--- src/main/java/phanisment/collection/entity/TesEntity.java
+++ src/main/java/phanisment/collection/entity/TesEntity.java
@@ -1,6 +1,6 @@
 package phanisment.collection.entity;
 
-import net.minecraft.entity.mob.PathAwareEntity
+import net.minecraft.entity.mob.PathAwareEntity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.world.World;
