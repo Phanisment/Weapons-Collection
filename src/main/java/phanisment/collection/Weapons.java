e60d60849c89cd2c4e183ba93600f3ab1f106a0f
Phanisment <agilseptiyono94@gmail.com>
2024-09-22 21:57:53 +0700

    Fix crash

--- src/main/java/phanisment/collection/Weapons.java
+++ src/main/java/phanisment/collection/Weapons.java
@@ -1,18 +1,35 @@
 package phanisment.collection;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
+import net.minecraft.entity.EntityDimensions;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.Registries;
+import net.minecraft.util.Identifier;
+import net.minecraft.registry.Registry;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import phanisment.collection.entity.EntityRegister;
+import phanisment.collection.entity.TesEntity;
 
 public class Weapons implements ModInitializer {
 	public static final String MOD_ID = "phanisment";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	public static final EntityType<TesEntity> TES = Registry.register(Registries.ENTITY_TYPE,
+		new Identifier(MOD_ID, "tes"),
+		FabricEntityTypeBuilder.create(SpawnGroup.MISC, TesEntity::new)
+			.dimensions(EntityDimensions.fixed(1.0f, 1.0f))
+			.trackRangeBlocks(80)
+			.trackedUpdateRate(3)
+			.build()
+	);
+
 	@Override
 	public void onInitialize() {
-		EntityRegister.registerEntities();
+		LOGGER.info("7 Sin's will come!");
 	}
 }
