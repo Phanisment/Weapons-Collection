2f9453134bb861d157bb1b3c040e86cf6af8d9c4
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-24 18:11:45 +0700

    Update TesModel.java

--- src/main/java/phanisment/collection/client/model/TesModel.java
+++ src/main/java/phanisment/collection/client/model/TesModel.java
@@ -4,7 +4,7 @@ import net.minecraft.client.render.entity.model.EntityModel;
 import net.minecraft.client.model.ModelPart;
 import net.minecraft.client.model.TexturedModelData;
 import net.minecraft.client.model.ModelPartData;
-import net.minecraft.client.util.math.MatrixStack
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.client.render.VertexConsumers;
 
 import phanisment.collection.entity.TesEntity;
