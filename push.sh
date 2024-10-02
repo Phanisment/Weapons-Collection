b2de342e94e64cfa97f79537ba0420ee14b226b5
Phanisment <agilseptiyono94@gmail.com>
2024-10-02 17:52:48 +0700

    Add Mechanic

--- push.sh
+++ push.sh
@@ -1,43 +1,22 @@
-#!/bin/bash
-
-# This script i really need because i use phone for coding this repository.
-
 if [ ! -d ".git" ]; then
-	echo "Error: This is not a Git repository."
+	echo "Error: This is not repository Git."
 	exit 1
-fi
-
-echo "Current git status:"
-git status
-
-if git diff-index --quiet HEAD --; then
-	echo "No changes to commit."
-	exit 0
 fi
 
 read -p "Commit Message: " commit_message
 
 git add .
 git commit -m "$commit_message"
-echo "Commited with message: $commit_message"
 
-read -p "Do you want to pull latest changes before pushing? (y/n): " pull_choice
-if [[ "$pull_choice" =~ ^[Yy]$ ]]; then
-	git pull origin "$branch_name" --rebase
-fi
+echo "Commited with message: $commit_message"
 
 read -p "Enter Branch Name (default: main): " branch_name
 branch_name=${branch_name:-main}
 
-read -p "Do you want to force push? (y/n): " force_push
-if [[ "$force_push" =~ ^[Yy]$ ]]; then
-	git push origin "$branch_name" --force
-else
-	git push origin "$branch_name"
-fi
+git push origin "$branch_name"
 
 if [ $? -eq 0 ]; then
-	echo "Push sendded to branch: $branch_name"
+	echo "Push to branch: $branch_name"
 else
-	echo "Error: Push failed."
+	echo "Error: Push Failed."
 fi
