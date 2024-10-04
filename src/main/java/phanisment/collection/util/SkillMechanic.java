3f4e687edcdc2b2ebc0fc0babf91f015ff2ed105
Phanisment <agilseptiyono94@gmail.com>
2024-10-04 20:38:28 +0700

    Fix :l

--- src/main/java/phanisment/collection/util/SkillMechanic.java
+++ src/main/java/phanisment/collection/util/SkillMechanic.java
@@ -5,13 +5,13 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.math.Vec3d;
 
 public class SkillMechanic {
-	public void leap(LivingEntity entity, double forward, double upward) {
+	public static void leap(LivingEntity entity, double forward, double upward) {
 		Vec3d lookVec = entity.getRotationVec(1.0F);
 		entity.addVelocity(lookVec.x * forward, upward, lookVec.z * forward);
 		entity.velocityModified = true;
 	}
 	
-	public void leapPlayer(PlayerEntity player, double forward, double upward) {
+	public static void leapPlayer(PlayerEntity player, double forward, double upward) {
 		Vec3d lookVec = player.getRotationVec(1.0F);
 		player.addVelocity(lookVec.x * forward, upward, lookVec.z * forward);
 		player.velocityModified = true;
