fec7fb01ce6bbb0eed207ee962abd219cc7ca60d
Phanisment <agilseptiyono94@gmail.com>
2024-10-05 14:38:42 +0700

    Fix

--- src/main/java/phanisment/collection/item/mechanic/HellfireTalonItem.java
+++ src/main/java/phanisment/collection/item/mechanic/HellfireTalonItem.java
@@ -9,9 +9,16 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.Rarity;
 import net.minecraft.world.World;
 
-import phanisment.collection.common.skill.BlazeLeapSkill;
+import phanisment.collection.util.TempVariableUtil;
+import phanisment.collection.util.mechanic.SkillMechanic;
+import phanisment.collection.entity.BlazeLeapEntity;
+import phanisment.collection.entity.RegisterEntities;
+
+import java.util.UUID;
 
 public class HellfireTalonItem extends SwordItem {
+	private TempVariableUtil<UUID, Boolean> isLeap = new TempVariableUtil<>();
+	
 	public HellfireTalonItem() {
 		super(ToolMaterials.NETHERITE, 12, -2.4F, new Settings().maxCount(1).rarity(Rarity.EPIC));
 	}
@@ -20,8 +27,23 @@ public class HellfireTalonItem extends SwordItem {
 	public TypedActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
 		ItemStack item = player.getStackInHand(hand);
 		if (!world.isClient) {
-			BlazeLeapSkill.run(world, player);
+			blazeLeapSkill(world, player);
 		}
 		return TypedActionResult.success(item);
+	}
+	
+	private void blazeLeapSkill(World world, PlayerEntity player) {
+		UUID playerId = player.getUuid(); 
+		if (player.isSneaking() && player.isOnGround()) {
+			BlazeLeapEntity vfx = new BlazeLeapEntity(RegisterEntities.BLAZE_LEAP, world);
+			vfx. setPosition(player.getX(), player.getY() + 0.1, player.getZ());
+			world.spawnEntity(vfx);
+			
+			SkillMechanic.leap(player, 0.2, 1.0);
+			isLeap.addTempVariable(playerId, true, 1000);
+		} 
+		else if (!player.isSneaking() && isLeap.containsKey(playerId)) {
+			SkillMechanic.jump(player, -10);
+		}
 	}
 }
