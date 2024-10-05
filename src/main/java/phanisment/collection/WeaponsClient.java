a33d8a9f8c26c7022ddb2e8c8bc2e7709e3a5929
Phanisment <agilseptiyono94@gmail.com>
2024-10-05 12:15:58 +0700

    Add More VFX

--- src/main/java/phanisment/collection/WeaponsClient.java
+++ src/main/java/phanisment/collection/WeaponsClient.java
@@ -6,14 +6,20 @@ import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 
 import phanisment.collection.client.RegisterModelLayers;
 import phanisment.collection.entity.RegisterEntities;
-import phanisment.collection.entity.FlameSlashEntity;
-import phanisment.collection.client.render.FlameSlashRender;
-import phanisment.collection.client.model.FlameSlashModel;
+import phanisment.collection.entity.*;
+import phanisment.collection.client.render.*;
+import phanisment.collection.client.model.*;
+import phanisment.collection.client.model.hellfiretalon.*;
 
 public class WeaponsClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
+		// Hellfire Talon | Slash
 		EntityRendererRegistry.register(RegisterEntities.FLAME_SLASH, FlameSlashRender::new);
 		EntityModelLayerRegistry.registerModelLayer(RegisterModelLayers.FLAME_SLASH, FlameSlashModel::getTexturedModelData);
+		
+		// Hellfire Talon | Blaze Leap
+		EntityRendererRegistry.register(RegisterEntities.BLAZE_LEAP, BlazeLeapRender::new);
+		EntityModelLayerRegistry.registerModelLayer(RegisterModelLayers.BLAZE_LEAP, BlazeLeapModel::getTexturedModelData);
 	}
 }
--- src/main/java/phanisment/collection/entity/RegisterEntities.java
+++ src/main/java/phanisment/collection/entity/RegisterEntities.java
@@ -14,6 +14,10 @@ import phanisment.collection.Weapons;
 public class RegisterEntities {
 	public static final EntityType<FlameSlashEntity> FLAME_SLASH = Registry.register(Registries.ENTITY_TYPE,
 		new Identifier(Weapons.MOD_ID, "flame_slash"),
-		FabricEntityTypeBuilder.create(SpawnGroup.MISC, FlameSlashEntity::new).dimensions(EntityDimensions.fixed(5.5f, 0.1f)).build()
+		FabricEntityTypeBuilder.create(SpawnGroup.MISC, FlameSlashEntity::new).dimensions(EntityDimensions.fixed(0f, 0f)).build()
+	);
+	public static final EntityType<BlazeLeapEntity> BLAZE_LEAP = Registry.register(Registries.ENTITY_TYPE,
+		new Identifier(Weapons.MOD_ID, "blaze_leap"),
+		FabricEntityTypeBuilder.create(SpawnGroup.MISC, BlazeLeapEntity::new).dimensions(EntityDimensions.fixed(0f, 0f)).build()
 	);
 }
