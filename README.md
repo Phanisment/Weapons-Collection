eae4e6f6699ae7c54455cc7e741a4a702a3e63e6
Phanisment <agilseptiyono94@gmail.com>
2024-10-06 14:10:18 +0700

    Eee

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - [ ] Add Curios/Trinket Items for passive Skill.
 - [ ] Balance Skill Items and Loot.
 - [ ] Make Custom Skill. (In working)
+- [ ] Mud Dungeon Structure.
--- src/main/java/phanisment/collection/mixin/ItemDisplayEntityAccessor.java
+++ src/main/java/phanisment/collection/mixin/ItemDisplayEntityAccessor.java
@@ -5,11 +5,8 @@ import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-@Mixin(ItemDisplayEntity.class)
+@Mixin(DisplayEntity.ItemDisplayEntity.class)
 public interface ItemDisplayEntityAccessor {
-	@Accessor("ItemStack")
+	@Accessor
 	ItemStack getItemStack();
-
-	@Accessor("ItemStack")
-	void setItemStack(ItemStack item);
 }
