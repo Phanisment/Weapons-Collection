8930abe340f7c778bda96225fdb4d32a23a68642
Phanisment <agilseptiyono94@gmail.com>
2024-10-06 10:37:22 +0700

    ...

--- src/main/java/phanisment/collection/mixin/ItemDisplayEntityAccessor.java
+++ src/main/java/phanisment/collection/mixin/ItemDisplayEntityAccessor.java
@@ -5,7 +5,7 @@ import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-@Mixin(ItemDisplayEntity.class)
+@Mixin(DisplayEntity.ItemDisplayEntity.class)
 public interface ItemDisplayEntityAccessor {
 	@Accessor("ItemStack")
 	ItemStack getItemStack();
