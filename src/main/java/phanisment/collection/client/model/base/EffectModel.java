a8b1f10b7ceec5306dcab98e1dc27e1f4838c908
Phanisment <agilseptiyono94@gmail.com>
2024-10-13 19:22:02 +0700

    K

--- src/main/java/phanisment/collection/client/model/base/EffectModel.java
+++ src/main/java/phanisment/collection/client/model/base/EffectModel.java
@@ -1,6 +1,6 @@
 package phanisment.collection.client.model.base;
 
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Identifier;
 
 import software.bernie.geckolib.model.DefaultedEntityGeoModel;
 
@@ -8,28 +8,28 @@ import phanisment.collection.common.entity.base.EffectEntity;
 import phanisment.collection.Weapons;
 
 public class EffectModel extends DefaultedEntityGeoModel<EffectEntity> {
-	private final ResourceLocation model;
-	private final ResourceLocation texture;
-	private final ResourceLocation animation;
+	private final Identifier model;
+	private final Identifier texture;
+	private final Identifier animation;
 
-	public EffectModel(ResourceLocation model, ResourceLocation texture, ResourceLocation animation) {
+	public EffectModel(Identifier model, Identifier texture, Identifier animation) {
 		this.model = model;
 		this.texture = texture;
 		this.animation = animation;
 	}
 
 	@Override
-	public ResourceLocation getModelResource(EffectModel model) {
+	public Identifier getModelResource(EffectModel model) {
 		return model;
 	}
 
 	@Override
-	public ResourceLocation getTextureResource(EffectModel texture) {
+	public Identifier getTextureResource(EffectModel texture) {
 		return texture;
 	}
 
 	@Override
-	public ResourceLocation getAnimationResource(EffectModel animation) {
+	public Identifier getAnimationResource(EffectModel animation) {
 		return animation;
 	}
 }
--- src/main/java/phanisment/collection/client/render/base/EffectRender.java
+++ src/main/java/phanisment/collection/client/render/base/EffectRender.java
@@ -1,34 +1,34 @@
 package phanisment.collection.client.render.base;
 
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Identifier;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
 import phanisment.collection.common.entity.base.EffectEntity;
 
 public class EffectRender extends GeoEntityRenderer<EffectEntity> {
-	private final ResourceLocation model;
-	private final ResourceLocation texture;
-	private final ResourceLocation animation;
+	private final Identifier model;
+	private final Identifier texture;
+	private final Identifier animation;
 
-	public EffectRender(EntityRendererProvider.Context render, ResourceLocation model, ResourceLocation texture, ResourceLocation animation) {
+	public EffectRender(EntityRendererProvider.Context render, Identifier model, Identifier texture, Identifier animation) {
 		super(render, new EffectModel(model, texture, animation));
 		this.model = model;
 		this.texture = texture;
 		this.animation = animation;
 	}
 
-	public ResourceLocation getModelResource(EffectRender model) {
+	public Identifier getModelResource(EffectRender model) {
 		return model;
 	}
 
-	public ResourceLocation getTextureResource(EffectRender texture) {
+	public Identifier getTextureResource(EffectRender texture) {
 		return texture;
 	}
 
-	public void setTextureResource(ResourceLocation texture) {
+	public void setTextureResource(Identifier texture) {
 		this.texture = texture;
 	}
 
-	public ResourceLocation getAnimationResource(EffectRender animation) {
+	public Identifier getAnimationResource(EffectRender animation) {
 		return animation;
 	}
 }
