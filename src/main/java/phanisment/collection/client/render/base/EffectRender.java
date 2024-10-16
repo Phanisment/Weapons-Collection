c08a48c43f445ece55bd902d062a39ea86950cda
Phanisment <agilseptiyono94@gmail.com>
2024-10-17 00:15:11 +0700

    e

--- src/main/java/phanisment/collection/client/render/base/EffectRender.java
+++ src/main/java/phanisment/collection/client/render/base/EffectRender.java
@@ -1,12 +1,12 @@
 package phanisment.collection.client.render.base;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.RenderLayer;
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.util.Identifier;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.VertexConsumer;
-import com.mojang.blaze3d.systems.RenderSystem;
 
 import org.jetbrains.annotations.Nullable;
 
@@ -14,7 +14,6 @@ import software.bernie.geckolib.renderer.GeoEntityRenderer;
 import software.bernie.geckolib.model.GeoModel;
 
 import phanisment.collection.common.entity.base.EffectEntity;
-import phanisment.collection.Weapons;
 
 public class EffectRender extends GeoEntityRenderer<EffectEntity> {
 	private Identifier model;
@@ -49,12 +48,12 @@ public class EffectRender extends GeoEntityRenderer<EffectEntity> {
 	}
 	
 	@Override
-	public void defaultRender(PoseStack poseStack, EffectEntity entity, MultiBufferSource bufferSource, @Nullable RenderType renderType, @Nullable VertexConsumer buffer, float yaw, float partialTick, int packedLight) {
-		super.defaultRender(poseStack, entity, bufferSource, renderType, buffer, yaw, partialTick, packedLight);
+	public void defaultRender(MatrixStack matrixStack, EffectEntity entity, VertexConsumerProvider vertexConsumerProvider, @Nullable RenderLayer renderLayer, @Nullable VertexConsumer vertexConsumer, float yaw, float partialTick, int packedLight) {
+		super.render(entity, yaw, partialTick, matrixStack, vertexConsumerProvider, packedLight);
 		RenderSystem.enableBlend();
 		RenderSystem.defaultBlendFunc();
-		VertexConsumer glowingBuffer = bufferSource.getBuffer(RenderType.getTranslucent());
-		super.defaultRender(poseStack, entity, glowingBuffer, null, yaw, partialTick, packedLight);
+		VertexConsumer glowingBuffer = vertexConsumerProvider.getBuffer(RenderLayer.getTranslucent());
+		super.render(entity, yaw, partialTick, matrixStack, vertexConsumerProvider, packedLight);
 		RenderSystem.disableBlend();
 	}
 }
